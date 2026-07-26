982f8219424111d3b7d231d35f174997ceeb2226
Yash Gautam <gyash1875@gmail.com>
2026-07-27 00:28:28 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js"
+import {connectDB} from "./config/db.js";
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
+dotenv.config();
+
+//console.log(process.env.MONGO_URI);
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+connectDB();
+
+// middleware
+app.use(cors({
+    origin:"http://localhost:5173",
+}));
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+
+app.use(rateLimiter);
+
+// our custom simple middleware
+
+// app.use((req, res, next) => {
+//     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+// })
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is running on PORT: ", PORT);
+    });
+});
